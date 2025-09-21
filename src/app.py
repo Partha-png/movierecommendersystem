a04c1085ce1f2745ccfa61f0a5f5e511709f8139
Partha-png <parthadash779@gmail.com>
2025-09-21 23:58:56 +0530

    added try catch

--- src/app.py
+++ src/app.py
@@ -3,12 +3,17 @@
 import streamlit as st
 from main import recommend_movies
 
-st.title("🎬 Mood-based Movie Recommender")
+try:
+    st.title("🎬 Mood-based Movie Recommender")
 
-if st.button("Detect Mood & Recommend Movies"):
-    with st.spinner('Detecting mood and finding best movies...'):
-        mood, recommendations = recommend_movies()
-    st.success(f"**Detected Mood:** `{mood}`")
-    st.subheader("🎥 Top Recommendations:")
-    for movie in recommendations:
-        st.write(f"✅ {movie}")
+    if st.button("Detect Mood & Recommend Movies"):
+        with st.spinner('Detecting mood and finding best movies...'):
+            mood, recommendations = recommend_movies()
+        st.success(f"**Detected Mood:** `{mood}`")
+        st.subheader("🎥 Top Recommendations:")
+        for movie in recommendations:
+            st.write(f"✅ {movie}")
+
+except Exception:
+    st.error("App crashed! See terminal for details.")
+    traceback.print_exc(file=sys.stderr)
